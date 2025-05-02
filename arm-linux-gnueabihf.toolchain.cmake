618f9a47d6300c2bb46d06a22f373ef3f2e67e15
Del Wang <wjg12341234@163.com>
2025-05-02 12:40:35 +0800

    fix: arm-linux-gnueabihf.toolchain.cmake

--- arm-linux-gnueabihf.toolchain.cmake
+++ arm-linux-gnueabihf.toolchain.cmake
@@ -2,8 +2,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7)
 
-set(CMAKE_C_COMPILER "arm-none-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "arm-none-linux-gnueabihf-g++")
+set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
